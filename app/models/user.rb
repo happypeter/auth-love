c0afb41096e3ee4e245fdddd434f69b8e77f0157
Peter Wang <happypeter1983@gmail.com>
2011-06-06 17:37:39 +0800

    i

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ActiveRecord::Base  
   attr_accessor :password  
+  before_save :encrypt_password  
+    
   validates_confirmation_of :password  
   validates_presence_of :password, :on => :create  
   validates_presence_of :email  
   validates_uniqueness_of :email  
+    
+  def encrypt_password  
+    if password.present?  
+      self.password_salt = BCrypt::Engine.generate_salt  
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)  
+    end  
+  end  
 end  
