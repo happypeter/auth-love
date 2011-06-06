41b8000db3efbc8acd60997206df9333b36413d2
Peter Wang <happypeter1983@gmail.com>
2011-06-06 16:44:55 +0800

    rails new auth-love

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthLove::Application.config.secret_token = '272705b0be0153cf15217694f3f93600f3cdb958569542432c7240bf294ab305674cb12fe725f10fac3de1f725fd4cb96851f4a4d91b3bfa9f61842f5c9e05a2'
