21a8c6f2eed4313801de4898b685048c330669d7
Peter Wang <happypeter1983@gmail.com>
2011-06-06 17:33:01 +0800

    i

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
-class User < ActiveRecord::Base
-end
+class User < ActiveRecord::Base  
+  attr_accessor :password  
+  validates_confirmation_of :password  
+  validates_presence_of :password, :on => :create  
+  validates_presence_of :email  
+  validates_uniqueness_of :email  
+end  
