ea00eb0eda67690962ecf5c423da8be9d04d4b43
Peter Wang <happypeter1983@gmail.com>
2011-06-12 23:32:51 +0800

    i

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create  
     user = User.authenticate(params[:email], params[:password])  
     if user  
-      session[:user_id] = user.id  
+      session[:user_id] = user.id # the session here, has nothing to do with the controller name Sessions, I will test this by using another controller name tommorrow! 
       redirect_to root_url, :notice => "Logged in!"  
     else  
       flash.now.alert = "Invalid email or password"  
