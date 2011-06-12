84b7715f22465d065e959b0424c64736f073a3df
Peter Wang <happypeter1983@gmail.com>
2011-06-12 22:02:13 +0800

    helper_method explained

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,3 +7,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]  
   end  
 end  
+#http://stackoverflow.com/questions/3549713/controller-helper-method
+#helper_method is useful when the functionality is something that's used between both the controller and the view. A good example is something like current_user.
+
+#If the method deals more with controller logic and not formatting then it belongs in the controller. Something like current_user would be shared between all controllers so it should be defined in the ApplicationController.
+
+#True "helper" methods deal with the view and handle things like formatting and template logic. These are seldom needed in the controller and they belong in their own module under app/helpers. You can include these in your controller when needed, but you end up with the whole module worth of view helper methods available to your controller.
+
