42ef756ee2de42ce71680a86e73ab2a621368ad0
Peter Wang <happypeter1983@gmail.com>
2011-06-16 16:59:41 +0800

    format

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ActiveRecord::Base
   validates_presence_of :email  
   validates_uniqueness_of :email  
 
-def self.authenticate(email, password)  
-  user = find_by_email(email)  
-  if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)  
-    user  
-  else  
-    nil  
+  def self.authenticate(email, password)  
+    user = find_by_email(email)  
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)  
+      user  
+    else  
+      nil  
+    end  
   end  
-end  
     
   def encrypt_password  
     if password.present?  
