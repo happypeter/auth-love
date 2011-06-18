09233a23e73398f56df41489b5ff4e0edf79696f
Peter Wang <happypeter1983@gmail.com>
2011-06-18 22:15:32 +0800

    small

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     # FIXME: this if sucks
     if current_user == nil
-      flash[:notice] = "Sorry, you are not allowed to edit this post"
+      flash[:notice] = "Sorry, you are not allowed to edit or destory this post"
       redirect_to posts_path
     else 
       if @post.user_id != current_user.id 
-        flash[:notice] = "Sorry, you are not allowed to edit this post"
+        flash[:notice] = "Sorry, you are not allowed to edit or destory this post"
         redirect_to posts_path
       end
     end
