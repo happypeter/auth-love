95fa24abd8432d7b08a98a87c96e7e9d86cb3f5a
Peter Wang <happypeter1983@gmail.com>
2011-06-18 17:33:40 +0800

    now I get what I wanted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110618092513) do
+ActiveRecord::Schema.define(:version => 20110618093046) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20110618092513) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
