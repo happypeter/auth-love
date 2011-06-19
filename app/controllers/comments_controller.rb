00b661009d2321e8e7ee33ead92ec17600043b07
Peter Wang <happypeter1983@gmail.com>
2011-06-19 21:28:44 +0800

    now user_id is saved correctly in comments table, but don't understand

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,16 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
-    #@comment.user_id = current_user.id if current_user
-    @comment.user_id = 4
-    redirect_to post_path(@post)
+    @comment.user_id = current_user.id if current_user
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to(@post, :notice => 'Post was successfully created.') }
+        format.xml  { render :xml => @post, :status => :created, :location => @post }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+      end
+    end
   end
  
   def destroy
