50ade2c112e703217c078f89fa45bcdd276bb4ab
Peter Wang <happypeter1983@gmail.com>
2011-06-20 10:52:36 +0800

    now post has author

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   validates :user_id,  :presence => true
   validates :title, :presence => true,
-                    :length => { :minimum => 5 }
+                    :length => { :minimum => 2 }
 
   has_many :comments, :dependent => :destroy
   belongs_to :user
