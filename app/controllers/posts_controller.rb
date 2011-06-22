279ce5a5b198a7a41ef564ac589763fb616bbbff
Peter Wang <happypeter1983@gmail.com>
2011-06-22 14:56:16 +0800

    dirty code again

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,6 @@ class PostsController < ApplicationController
   # GET /posts/1.xml
   def show
     @post = Post.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @post }
