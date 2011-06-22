983c831367aee173a3a42ebe47500ad0d396127d
Peter Wang <happypeter1983@gmail.com>
2011-06-22 16:01:15 +0800

    here I copy idea from app/views/posts/show.html.erb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   validates :user_id, :body,  :presence => true
   belongs_to :post
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessor :password  
   before_save :encrypt_password  
   has_many :posts
+  has_many :comments
     
   validates_confirmation_of :password  
   validates_presence_of :password, :on => :create  
