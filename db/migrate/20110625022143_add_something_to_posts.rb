7f52a68287a581a794de91bfd3ed6db16f754c09
Peter Wang <happypeter1983@gmail.com>
2011-06-25 10:22:00 +0800

    AGAIN:  rails generate migration AddSomethingToPosts user_id:integer
    for mysql this time

---
+++ db/migrate/20110625022143_add_something_to_posts.rb
@@ -0,0 +1,9 @@
+class AddSomethingToPosts < ActiveRecord::Migration
+  def self.up
+    add_column :posts, :user_id, :integer
+  end
+
+  def self.down
+    remove_column :posts, :user_id
+  end
+end
