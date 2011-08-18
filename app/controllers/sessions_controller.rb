ab1083a00e4171f9835c5ab4c9ec44e79e394612
Peter Wang <happypeter1983@gmail.com>
2011-08-18 15:33:14 +0800

    delete the obvious

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,13 @@ class SessionsController < ApplicationController
   end  
     
   def create  
-  # called when "post sessions"
     user = User.authenticate(params[:name], params[:password])  
     if user  
-      session[:user_id] = user.id # the session here, has nothing to do with the controller name Sessions, I will test this by using another controller name tommorrow! 
+      session[:user_id] = user.id 
       redirect_to root_url, :notice => "Logged in!"  
     else  
       flash.now.alert = "Invalid name or password"  
-      render "new" # new => ../views/sessions/new.html.er 
+      render "new" 
     end  
   end  
 
