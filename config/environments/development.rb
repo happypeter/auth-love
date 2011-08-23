cafae58031f0c2e42d9fd98b5f2b2bfb0be0046b
Peter Wang <happypeter1983@gmail.com>
2011-08-23 15:32:11 +0800

    copy this line from railscasts/

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,7 @@ AuthLove::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.delivery_method = :sendmail
 end
 
