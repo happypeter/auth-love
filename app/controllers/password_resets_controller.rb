ea53f4182b3c77c50602889ec783d9a60b1503f3
Peter Wang <happypeter1983@gmail.com>
2011-09-03 22:25:05 +0800

    if the Email is not registered, issue a warning

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,8 +3,12 @@ class PasswordResetsController < ApplicationController
   end
   def create
     user = User.find_by_email(params[:email])
-    user.send_password_reset if user
-    redirect_to root_url, :notice => "Email sent with password reset instructions."
+    if user
+      user.send_password_reset 
+      redirect_to root_url, :notice => "Email sent with password reset instructions."
+    else
+      redirect_to root_url, :notice => "No such email address!"
+    end
   end
 
   def edit
