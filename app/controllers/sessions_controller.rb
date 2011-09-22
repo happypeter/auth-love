f0194ec8ff33be5fbdf53faad3e3e993b34674df
Peter Wang <happypeter1983@gmail.com>
2011-09-22 19:28:20 +0800

    now click reply when not login, it works like HN
    first you get the login page, then you are redirect to page with
    parent comment
    a reply form
    
    NOT
    
    the post#show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,12 @@ class SessionsController < ApplicationController
   def new  
     session[:return_to] = nil
     session[:return_to] = params[:return_to] if params[:return_to]
+
+    # for reply
+    session[:parent_id] = nil
+    session[:parent_id] = params[:parent_id] if params[:parent_id]
+    session[:post_id] = nil
+    session[:post_id] = params[:post_id] if params[:post_id]
   end  
     
   def create  
@@ -13,7 +19,6 @@ class SessionsController < ApplicationController
         else
           cookies[:auth_token] = user.auth_token
         end
-        redirect_to_target_or_default root_url, :notice => "Signed in successfully"
       else # for old users who have no auth_token
         user.generate_token(:auth_token)
         user.save
@@ -22,6 +27,10 @@ class SessionsController < ApplicationController
         else
           cookies[:auth_token] = user.auth_token
         end
+      end
+      if session[:parent_id] && session[:post_id] # for reply
+        redirect_to(new_comment_path( :post_id => session[:post_id], :parent_id => session[:parent_id]), :notice =>"Signed in")   
+      else # for other login case
         redirect_to_target_or_default root_url, :notice => "Signed in successfully"
       end
     else  
