6b05609ec60e0c6ceae14c51d73d58bd41806c08
Peter Wang <happypeter1983@gmail.com>
2012-03-26 19:22:14 +0800

    I do not need to install pygment anymore.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,6 @@ module ApplicationHelper
     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
     Redcarpet.new(text, *options).to_html.html_safe
   end
-
-  def syntax_highlighter(html)
-    #DO install pygment first, bundler won't take care of this for you
-    doc = Nokogiri::HTML(html)
-    doc.search("//pre[@lang]").each do |pre|
-      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
-    end
-    doc.to_s
-  end
 end
 
 
