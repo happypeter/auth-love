d87ea5195e8c6eb79d8eca48f6307023d744ddfb
Peter Wang <happypeter1983@gmail.com>
2012-03-26 19:21:08 +0800

    syntax_highlighter is not necessary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def markdown(text)
     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
-    syntax_highlighter(Redcarpet.new(text, *options).to_html).html_safe
+    Redcarpet.new(text, *options).to_html.html_safe
   end
 
   def syntax_highlighter(html)
